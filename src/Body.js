3c43a9fe1d04af9119b8ae2f3e47bf6423057c5c
Jamie <drpcom@yahoo.com>
2021-12-03 22:49:16 -0600

    Added die.

--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Body.css';
 import Sidebar from './Sidebar';
+import CasinoIcon from '@material-ui/icons/Casino';
 
 const Body = () => {
     return (
         <div className='bodyContainer'>
+            <div className="dice">
+            <CasinoIcon />
+            <h1>Roll!</h1>
+            </div>
             <div className='items'>
                 <h1>Random Item 1</h1>
                 <h1>Random Item 2</h1>
@@ -12,7 +17,7 @@ const Body = () => {
                 <h1>Random Item 4</h1>
                 <h1>Random Item 5</h1>
             </div>
-        <Sidebar />
+            <Sidebar />
         </div>
     )
 }
