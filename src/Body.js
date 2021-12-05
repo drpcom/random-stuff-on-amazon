65651dfba0ed27324dadb993bdc427f0c9846cbe
Jamie <drpcom@yahoo.com>
2021-12-05 14:06:16 -0600

    Added rating stars.

--- src/Body.js
+++ src/Body.js
@@ -4,6 +4,12 @@ import Sidebar from './Sidebar';
 import CasinoIcon from '@material-ui/icons/Casino';
 import RandomItem from './RandomItem';
 
+import hotwheel from './images/hotwheel.jpg';
+import sweater from './images/sweater.jpg';
+import chocolates from './images/chocolates.jpg';
+import socks from './images/socks.jpg';
+import oculus from './images/oculus.jpg';
+
 const Body = () => {
     return (
         <div className='bodyContainer'>
@@ -13,29 +19,34 @@ const Body = () => {
             </div>
             <div className='items'>
                 <RandomItem 
+                pic={hotwheel} 
                 title="Hotwheel Set" 
-                pic="" 
                 desc="This is an Ocean Avenue hotwheel set." 
+                rating="⭐⭐⭐⭐⭐"
                 price="$19.99" />
                 <RandomItem 
+                pic={sweater} 
                 title="Sweater" 
-                pic="" 
                 desc="The most comfortable sweater money can buy." 
+                rating="⭐⭐⭐⭐⭐"
                 price="$38.99" />
                 <RandomItem 
+                pic={chocolates}
                 title="Chocolate Candy" 
-                pic="" 
                 desc="Perfect for holiday season." 
+                rating="⭐⭐⭐⭐⭐"
                 price="$5.99" />
                 <RandomItem 
+                pic={socks} 
                 title="Socks" 
-                pic="" 
-                desc="You get them as a gift every year. You know what this is." 
+                desc="You get them as a gift every year. You know what this is."
+                rating="⭐⭐⭐⭐⭐" 
                 price="$10.99" />
                 <RandomItem 
+                pic={oculus}
                 title="Oculus Rift" 
-                pic="" 
                 desc="This is the cool item." 
+                rating="⭐⭐⭐⭐⭐"
                 price="$249.99" />
             </div>
             <Sidebar />
--- src/RandomItem.js
+++ src/RandomItem.js
@@ -4,9 +4,10 @@ import './RandomItem.css';
 const RandomItem = (props) => {
     return (
         <div className="randomItemContainer">
+            <img src={props.pic} alt={props.title} />
             <h1>{props.title}</h1>
-            <h1>{props.pic}</h1>
             <h1>{props.desc}</h1>
+            <h1>{props.rating}</h1>
             <h1>{props.price}</h1>
         </div>
     )
