bf3019a88ac72a244363d6d9702316e8c1ef173d
Jamie <drpcom@yahoo.com>
2021-12-11 17:10:29 -0600

    Finished implementing random products from FakeStoreApi.

--- src/Body.js
+++ src/Body.js
@@ -12,7 +12,25 @@ const Body = () => {
         .then((res)=> res.json())
         .then((json)=> setProducts(json))
     },[]);
-    console.log(products);
+
+    function shuffle(array) {
+        let currentIndex = array.length,  randomIndex;
+      
+        // While there remain elements to shuffle...
+        while (currentIndex) {
+      
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          // And swap it with the current element.
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+      
+        return array;
+      }
+      shuffle(products);
 
     return (
         <div className='bodyContainer'>
